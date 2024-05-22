66e14d04c656b3c879c75b1f7c621c9c95e2517c
EZRA-DVLPR <derpisontr@gmail.com>
2024-05-21 21:04:04 -0700

    postbanner template modified: added date, tags, author to page display

--- my_website/layouts/PostBanner.tsx
+++ my_website/layouts/PostBanner.tsx
@@ -9,6 +9,14 @@ import PageTitle from '@/components/PageTitle'
 import SectionContainer from '@/components/SectionContainer'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import Tag from '@/components/Tag'
+
+const postDateTemplate: Intl.DateTimeFormatOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+}
 
 interface LayoutProps {
   authorDetails: CoreContent<Authors>[]
@@ -19,61 +27,85 @@ interface LayoutProps {
 }
 
 export default function PostMinimal({ content, next, prev, authorDetails, children }: LayoutProps) {
-  const { slug, title, images } = content
-  const displayImage = images && images.length > 0 ? images[0] : 'https://picsum.photos/seed/picsum/800/400'
+  const { slug, title, images, date, tags } = content
+  
+  //find a better image suited for a default display
+  const displayImage = images && images.length > 0 ? images[0] : '/static/images/github-traffic.png'
 
   return (
     <SectionContainer>
       <ScrollTopAndComment />
       <article>
-        <div>
-          <div className="space-y-1 pb-10 text-center dark:border-gray-700">
-            <div className="w-full">
-              <Bleed>
-                <div className="relative aspect-[2/1] w-full">
-                  <Image src={displayImage} alt={title} fill className="object-cover" />
-                </div>
-              </Bleed>
-            </div>
-            <div className="relative pt-10">
-              <PageTitle>{title}</PageTitle>
-            </div>
-            <br></br>
-              <dd>
-                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:space-x-0 xl:space-y-8">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width={38}
-                          height={38}
-                          alt="avatar"
-                          className="h-10 w-10 rounded-full"
-                        />
-                      )}
-                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                        <dt className="sr-only">Name</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
+      <div>
+        <div className="flex justify-between items-center pt-10">
+          <div>
+            <PageTitle>{title}</PageTitle>
           </div>
+          <div>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+              <time dateTime={date}> 
+                {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+              </time>
+            </dd>
+          </div>
+        </div>
+        <div className="space-y-1 pb-10 text-center dark:border-gray-700">
+          <div className="w-full">
+            <Bleed>
+              <div className="relative aspect-[2/1] w-full">
+                <Image src={displayImage} alt={title} fill className="object-cover" />
+              </div>
+            </Bleed>
+          </div>
+          <br></br>
+            <dd>
+              <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:space-x-0 xl:space-y-8">
+                {authorDetails.map((author) => (
+                  <li className="flex items-center space-x-2" key={author.name}>
+                    {author.avatar && (
+                      <Image
+                        src={author.avatar}
+                        width={38}
+                        height={38}
+                        alt="avatar"
+                        className="h-10 w-10 rounded-full"
+                      />
+                    )}
+                    <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                      <dt className="sr-only">Name</dt>
+                      <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
+                      <dt className="sr-only">Twitter</dt>
+                      <dd>
+                        {author.twitter && (
+                          <Link
+                            href={author.twitter}
+                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          >
+                            {author.twitter
+                              .replace('https://twitter.com/', '@')
+                              .replace('https://x.com/', '@')}
+                          </Link>
+                        )}
+                      </dd>
+                    </dl>
+                  </li>
+                ))}
+              </ul>
+            </dd>
+        </div>
+          {tags && (
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xl uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
+                    </div>
+                  </div>
+                )}
           <div className="prose max-w-none py-4 dark:prose-invert">{children}</div>
           {siteMetadata.comments && (
             <div className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300" id="comment">
@@ -103,7 +135,6 @@ export default function PostMinimal({ content, next, prev, authorDetails, childr
                     {next.title} &rarr;
                   </Link>
                 </div>
-                
               )}
             </div>
           </footer>
