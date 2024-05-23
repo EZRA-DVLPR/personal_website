c8c13c9d2a6690ccbbde6ccf2582f56bbbf5faeb
EZRA-DVLPR <derpisontr@gmail.com>
2024-05-22 18:59:21 -0700

    removed all blog posts and obtained a single blog post containing useful info. updated newsletter text. removed projects

--- my_website/data/projectsData.ts
+++ my_website/data/projectsData.ts
@@ -7,33 +7,13 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    status: 'In Progress',
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    status: 'Completed',
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-  {
-    title: 'The Time Machine 2',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    status: 'Planned',
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // {
+  //   title: 'Title text to display',
+  //   description: `Text to display`,
+  //   status: 'Planned' | 'Completed' | 'In Progress',
+  //   imgSrc: '/static/images/IMG',                            optional
+  //   href: '/blog/BLOGPAGENAME',                              optional
+  // },
 ]
 
 export default projectsData
