46ed282cf6fcb116ff97a9af3cce9f4c85b30db0
EZRA-DVLPR <derpisontr@gmail.com>
2024-05-29 20:43:00 -0700

    added construction component and updated image for profile

--- my_website/app/game/page.tsx
+++ my_website/app/game/page.tsx
@@ -1,4 +1,5 @@
 import { genPageMetadata } from 'app/seo'
+import Construction from '@/components/Construction'
 
 export const metadata = genPageMetadata({ title: 'Games' })
 
@@ -9,7 +10,8 @@ export default function Projects() {
         <h1 className="mb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           List of Games
         </h1>
-        Work in Progress! Check back later!
+        <p className='m-4'>Work in Progress! Check back later!</p>
+        <Construction />
       </div>
     </>
   )
---
+++ my_website/components/Construction.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Construction = () => {
+  return (
+    <>
+      <img src="/static/images/under_construction.jpg"></img>
+      <p className='mt-4'>
+        <a href="https://www.pexels.com/photo/under-construction-signage-on-laptop-keyboard-211122/">
+        Photo Credit: Photo by Fernando Arcos from Pexels
+        </a>
+      </p>
+      <div className="mt-6 flex items-center justify-center text-4xl">
+        <p>Coding in progress...</p>
+      </div>
+    </>
+  )
+}
+
+export default Construction
