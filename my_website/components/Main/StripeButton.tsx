e2ddb984950dd31a39a10d9f9ac338238384adad
EZRA-DVLPR <derpisontr@gmail.com>
2025-03-08 12:45:32 -0800

    dangerously add HTML cuz JSX doesnt recognize stripe buy button

--- my_website/components/Main/StripeButton.tsx
+++ my_website/components/Main/StripeButton.tsx
@@ -7,19 +7,25 @@ const StripeButton = () => {
 		const script = document.createElement("script");
 		script.src = "https://js.stripe.com/v3/buy-button.js";
 		script.async = true;
-		document.body.appendChild(script);
+		document.body.appendchild(script);
 
 		return () => {
-			document.body.removeChild(script);
+			document.body.removechild(script);
 		};
 	}, []);
 
 	return (
 		<div>
-			<stripe-buy-button
-				buy-button-id="buy_btn_1R0IFIFG5nxP1asba7SdmVpK"
-				publishable-key="pk_live_51R0GQtFG5nxP1asbGtZSqJvCZ7j6KpnVXz0Fc8n3qfFSGNkphDHm1pOgH11N0DYLMTyq3tZKiGzBi61EvhCMJI6000nxS4K20b"
-			></stripe-buy-button>
+			<div
+				dangerouslySetInnerHTML={{
+					__html: `
+          <stripe-buy-button
+            buy-button-id="buy_btn_1r0ififg5nxp1asba7sdmvpk"
+            publishable-key="pk_live_51r0gqtfg5nxp1asbgtzsqjvcz7j6kpnvxz0fc8n3qffsgnkphdhm1pogh11n0dylmtyq3tzkigzbi61evhcmji6000nxs4k20b"
+          ></stripe-buy-button>
+        `,
+				}}
+			/>
 		</div>
 	);
 };
